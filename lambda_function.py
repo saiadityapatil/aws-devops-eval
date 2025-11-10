8dc7d579695c6ce32925f76f8d3923bb6a2ad566
saiadityapatil <1rn22cd080.saiadityapatil@gmail.com>
2025-11-10 15:19:11 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,16 +6,16 @@ table = dynamodb.Table('Emp_Master')
 
 def lambda_handler(event, context):
     if event['requestContext']['http']['method'] == 'POST':
-        try:
-            table.put_item(Item=json.loads(event['body']))
-            return {
-                'statusCode': 200,
-                'body': json.dumps('Record added!! Great!!')
-            }
+        table.put_item(Item=json.loads(event['body']))
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Record added!! Great!!')
+        }
     elif event['requestContext']['http']['method'] == 'GET':
         response = table.get_item(Key={'Emp_Id': event['queryStringParameters']['emp_id']})
         return {
             'statusCode': 200,
             'body': json.dumps(response['Item'])
         }
+
 
