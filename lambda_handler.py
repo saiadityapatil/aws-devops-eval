2998c934539e4a322ebb024d79e7506411c39f16
saiadityapatil <1rn22cd080.saiadityapatil@gmail.com>
2025-11-10 14:26:16 +0530

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,19 +1,20 @@
+import json
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-table_name = 'Emp_Master'
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Emp_Master')
 
 def lambda_handler(event, context):
-    table = dynamodb.Table(table_name)
-    operation = event['httpMethod']
-    payload = event['payload']
-    if operation == 'POST':
-        response = table.put_item(Item=payload)
-        return {'statusCode': 200, 'body': 'Item added successfully'}
-    elif operation == 'GET':
-        response = table.get_item(Key=payload)
-        return {'statusCode': 200, 'body': response['Item']}
-    else:
-        return {'statusCode': 400, 'body': 'Invalid operation'}
-
-print("hello")
+    if event['requestContext']['http']['method'] == 'POST':
+        try:
+            table.put_item(Item=json.loads(event['body']))
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Record added')
+            }
+    elif event['requestContext']['http']['method'] == 'GET':
+        response = table.get_item(Key={'Emp_Id': event['queryStringParameters']['emp_id']})
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response['Item'])
+        }
